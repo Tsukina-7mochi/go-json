8c0a7695e4fd98baad3b210671c6e05bf9c72410
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-03-23 18:45:37 +0900

    feat: implement decoder for numbers

--- decoder/decode.go
+++ decoder/decode.go
@@ -21,6 +21,21 @@ func decodeString(t *tokenizer.Tokenizer, out *string) error {
 	return nil
 }
 
+func decodeNumber(t *tokenizer.Tokenizer, out *float64) error {
+	token, err := t.Next()
+	if err != nil {
+		return err
+	}
+
+	if token.Kind == tokenizer.NumberToken {
+		*out = token.FloatValue()
+	} else {
+		return errors.New("Expected number token")
+	}
+
+	return nil
+}
+
 func Decode(input []byte, out any) error {
 	outTypePtr := reflect.TypeOf(out)
 	if outTypePtr.Kind() != reflect.Ptr {
@@ -32,6 +47,13 @@ func Decode(input []byte, out any) error {
 	switch outType.Kind() {
 	case reflect.String:
 		return decodeString(tokenizer.NewTokenizer(input), out.(*string))
+	case reflect.Int:
+		var num float64
+		err := decodeNumber(tokenizer.NewTokenizer(input), &num)
+		*out.(*int) = int(num)
+		return err
+	case reflect.Float64:
+		return decodeNumber(tokenizer.NewTokenizer(input), out.(*float64))
 	}
 
 	return errors.New("Unsupported type")
--- decoder/decode_test.go
+++ decoder/decode_test.go
@@ -12,3 +12,25 @@ func TestDecode_String(t *testing.T) {
 		t.Errorf("Decode == %q, want %q", str, "hello")
 	}
 }
+
+func TestDecode_Int(t *testing.T) {
+	var num int
+	err := Decode([]byte(`123`), &num)
+	if err != nil {
+		t.Fatalf("Decode returned error: %v", err)
+	}
+	if num != 123 {
+		t.Errorf("Decode == %v, want %v", num, 123)
+	}
+}
+
+func TestDecode_Float64(t *testing.T) {
+	var num float64
+	err := Decode([]byte(`0.25`), &num)
+	if err != nil {
+		t.Fatalf("Decode returned error: %v", err)
+	}
+	if num != 0.25 {
+		t.Errorf("Decode == %v, want %v", num, 0.25)
+	}
+}
