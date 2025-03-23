b161ea902a6debdd6f9e3837563a11d093919a35
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-03-23 18:47:10 +0900

    test: refactor tests

--- decoder/decode_test.go
+++ decoder/decode_test.go
@@ -3,34 +3,40 @@ package decoder
 import "testing"
 
 func TestDecode_String(t *testing.T) {
-	var str string
-	err := Decode([]byte(`"hello"`), &str)
+	want := "hello"
+	var got string
+	err := Decode([]byte(`"hello"`), &got)
+
 	if err != nil {
 		t.Fatalf("Decode returned error: %v", err)
 	}
-	if str != "hello" {
-		t.Errorf("Decode == %q, want %q", str, "hello")
+	if got != want {
+		t.Errorf("Decode == %q, want %q", got, want)
 	}
 }
 
 func TestDecode_Int(t *testing.T) {
+	want := 123
 	var num int
 	err := Decode([]byte(`123`), &num)
+
 	if err != nil {
 		t.Fatalf("Decode returned error: %v", err)
 	}
-	if num != 123 {
-		t.Errorf("Decode == %v, want %v", num, 123)
+	if num != want {
+		t.Errorf("Decode == %v, want %v", num, want)
 	}
 }
 
 func TestDecode_Float64(t *testing.T) {
+	want := 0.25
 	var num float64
 	err := Decode([]byte(`0.25`), &num)
+
 	if err != nil {
 		t.Fatalf("Decode returned error: %v", err)
 	}
-	if num != 0.25 {
-		t.Errorf("Decode == %v, want %v", num, 0.25)
+	if num != want {
+		t.Errorf("Decode == %v, want %v", num, want)
 	}
 }
