aa13692aa9c81a1703ba5d342c573651c31c4da9
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-03-25 16:16:22 +0900

    feat: add example

---
+++ example/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"json/decoder"
+)
+
+type Address struct {
+	Street string
+	City   string
+	State  string
+	Zip    string `json:"zip_code"`
+}
+
+type User struct {
+	ID        int `json:"id"`
+	Name      string
+	Email     string
+	Addresses []Address
+}
+
+func printUser(user User) {
+	fmt.Printf("ID: %d\n", user.ID)
+	fmt.Printf("Name: %s\n", user.Name)
+	fmt.Printf("Email: %s\n", user.Email)
+
+	for i, address := range user.Addresses {
+		fmt.Printf("Address %d: %s, %s, %s %s\n", i+1, address.Street, address.City, address.State, address.Zip)
+	}
+}
+
+func main() {
+	input := `{
+        "id": 1,
+        "name": "John Doe",
+        "email": "johndoe@example.com",
+        "addresses": [
+            {
+                "street": "123 Main St",
+                "city": "Springfield",
+                "state": "IL",
+                "zip_code": "62701"
+            },
+            {
+                "street": "456 Elm St",
+                "city": "Springfield",
+                "state": "IL",
+                "zip_code": "62701"
+            }
+        ]
+    }`
+
+	var user User
+	err := decoder.Decode([]byte(input), &user)
+	if err != nil {
+		panic(err)
+	}
+
+	printUser(user)
+}
